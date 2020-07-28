2e5f2ea37becbe1b5853258d24bed64fc886e262
Denis Kuznetsov <deniskuzn@gmail.com>
2020-07-28 23:24:45 +0300

    added data types

---
+++ obj/src/version1/PaymentMethodTypeV1.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class PaymentMethodTypeV1 {
+}
+exports.PaymentMethodTypeV1 = PaymentMethodTypeV1;
+PaymentMethodTypeV1.Card = "card";
+PaymentMethodTypeV1.BankAccount = "account";
+//# sourceMappingURL=PaymentMethodTypeV1.js.map
